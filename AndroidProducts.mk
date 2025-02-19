d56505e2022301bbf595df5fc1f7b507cda0addd
Shorif Uddin <gooogleabal@gmail.com>
2025-02-19 16:25:27 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,12 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_NX669J.mk
+    $(LOCAL_DIR)/twrp_NX669J.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_NX669J-user \
-    omni_NX669J-userdebug \
-    omni_NX669J-eng
+    twrp_NX669J-user \
+    twrp_NX669J-userdebug \
+    twrp_NX669J-eng
