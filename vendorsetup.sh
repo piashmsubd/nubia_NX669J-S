e9bf8b4b7ba3540b65b14f869329886c05e79d21
Shorif Uddin <gooogleabal@gmail.com>
2025-02-19 16:26:44 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,8 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_NX669J-user
-add_lunch_combo omni_NX669J-userdebug
-add_lunch_combo omni_NX669J-eng
+add_lunch_combo twrp_NX669J-user
+add_lunch_combo twrp_NX669J-userdebug
+add_lunch_combo twrp_NX669J-eng
